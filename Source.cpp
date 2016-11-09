58dc6a8b57d5d743d317cc22ddcfa34390f9b4cb
Roman <roman.dronov1@gmail.com>
2016-11-09 00:13:53 -0800

    Update Source.cpp
    added while

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,25 @@ using std::cout;
 using std::cin;
 using std::endl;
 int main() {
-	int num, sys, num10;
-	cout << "Enter the number that you want to convert: ";
-	cin >> num;
-	cout<< "Enter the system in which it shoud be converted: ";
-	cin >> sys;
-	cout << endl << "Here is your nuber in " << sys << " system: "<<endl;
-	while (num > 0) {
-		num10 = num % sys;
-		cout << num10;
-		num = num / sys;
+	int num, sys, number;
+	char answer = 'Y';
+	while (answer == 'Y' || answer == 'y') {
+		cout << "Enter the number that you want to convert: ";
+		cin >> num;
+		cout << "Enter the system in which it shoud be converted: ";
+		cin >> sys;
+			cout << endl << "Here is your uber in " << sys << " system: " << endl;
+		while (num > 0) {
+			number = num % sys;
+			cout << number;
+			num = num / sys;
+		}
+		cout << endl;
+		cout << "If you want to countinue type Y or y:";
+		cin >> answer;
+		cout << endl;
 	}
+
+	system("pause");
 	return 0;
 }
